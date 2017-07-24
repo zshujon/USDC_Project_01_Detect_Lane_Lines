56ff01e0b15a57ff9b8978dcb511297a5c35a08d
Md. Zakir Hossan <zshujon@gmail.com>
2017-07-25 03:24:53 +0600

    necessary files

--- sdc_ld_00.py
+++ sdc_ld_00.py
@@ -5,7 +5,8 @@ Created on Mon Jul 24 20:37:24 2017
 @author: ZSHUJON
 """
 
-from utils import *
+from utils import imread, plt
 
 road_image = imread('test_images/solidYellowCurve2.jpg')
+
 plt.imshow(road_image)
