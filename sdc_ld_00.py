21bb94d4d1b0e0285c5c41f8dea3262a77f67e77
zshujon <zshujon@gmail.com>
2017-07-24 20:49:32 +0600

    necesseary files

---
+++ sdc_ld_00.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 24 20:37:24 2017
+
+@author: ZSHUJON
+"""
+
+from utils import *
+
+road_image = imread('test_images/solidYellowCurve2.jpg')
+plt.imshow(road_image)
